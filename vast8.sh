e1e284feb0bcd99a658d8dc5172af39621aae445
wildhdmicable <154681613+wildhdmicable@users.noreply.github.com>
2023-12-27 21:27:43 +0100

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xbD00d38bb7F89E9ac89b80Ef6CC177c9FCdcB36D/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
@@ -48,10 +48,6 @@ sleep 1
 sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
 sleep 1
 sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
 
 echo "-------------------------"
 echo "    MINING  XENBLOCKS    "
