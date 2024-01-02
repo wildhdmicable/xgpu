c7a2fded4f6a1491e4ae87fd180b7f66f2b30df5
wildhdmicable <154681613+wildhdmicable@users.noreply.github.com>
2024-01-02 09:56:13 +0100

    Update vast12.sh

--- vast12.sh
+++ vast12.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xbd00d38bb7f89e9ac89b80ef6cc177c9fcdcb36d/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xbD00d38bb7F89E9ac89b80Ef6CC177c9FCdcB36D/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
