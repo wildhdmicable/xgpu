a4823aa7c001e98c1ccfe657f6fa61f6a5018c97
wildhdmicable <154681613+wildhdmicable@users.noreply.github.com>
2024-01-02 07:52:22 +0100

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -42,6 +42,12 @@ sleep 1
 sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
 sleep 1
 sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
+sleep 1
+sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
+sleep 1
+sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
+sleep 1
+sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
 
 echo "-------------------------"
 echo "    MINING  XENBLOCKS    "
